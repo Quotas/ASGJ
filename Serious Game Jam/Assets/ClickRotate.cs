ec3c150090221eda506932de72d4bad589d35067
Seamus <seamussalt@gmail.com>
2017-05-17 11:07:17 +0100

    Fixed initial commit

--- Serious Game Jam/Assets/ClickRotate.cs
+++ Serious Game Jam/Assets/ClickRotate.cs
@@ -2,15 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ClickRotate : MonoBehaviour {
+public class ClickRotate : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        while (Input.GetMouseButtonDown(0))
+        {
+
+            if (Input.GetAxisRaw("Mouse X") >= 0)
+            {
+
+
+            }
+            else if (Input.GetAxisRaw("Mouse X") < 0)
+            {
+
+
+            }
+
+
+
+
+        }
+
+
+
+
+
+    }
 }
