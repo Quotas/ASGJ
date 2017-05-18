cc6f1c9d1ad4fce99cac118cbebee4e0d9499c6a
Seamus <seamussalt@gmail.com>
2017-05-18 15:06:41 +0100

    Multi items scenes

--- Serious Game Jam/Assets/Scripts/GameManager.cs
+++ Serious Game Jam/Assets/Scripts/GameManager.cs
@@ -6,15 +6,106 @@ public class GameManager : MonoBehaviour
 {
 
 
+
+    public class Delivery
+    {
+
+        //Holds all the dialgue for the game
+        public List<Dialogue> sceneDialogue = new List<Dialogue>();
+
+        public List<GameObject> items = new List<GameObject>();
+        public bool active;
+        public string name;
+        public GameObject boss;
+
+        public void AddDialogue(string _text, ref GameObject _entity)
+        {
+
+            sceneDialogue.Add(new Dialogue(_text, ref _entity, sceneDialogue.Count));
+
+        }
+
+        public void AddItem(GameObject item)
+        {
+
+
+            items.Add(item);
+
+
+        }
+
+        public bool ProcessDialogue(int index)
+        {
+
+            if (index > sceneDialogue.Count - 1)
+            {
+                return false;
+            }
+            sceneDialogue[index].Fire();
+            return true;
+
+        }
+
+        public bool SetActiveItem(int index)
+        {
+            if (index > items.Count - 1)
+            {
+                return false;
+            }
+            else
+            {
+                items[index].gameObject.SetActive(true);
+                return true;
+            }
+
+
+        }
+    }
+
+    public struct Dialogue
+    {
+
+        public string text { get; set; }
+        int id { get; set; }
+        GameObject actor;
+
+        public Dialogue(string _text, ref GameObject _entity, int _id)
+        {
+
+            text = _text;
+            id = _id;
+            actor = _entity;
+
+
+
+        }
+
+
+        public void Fire()
+        {
+            actor.GetComponent<BossController>().BossEnter();
+            actor.GetComponent<BossController>().BossSpeaks(text);
+
+        }
+    }
+
+
     //UI Elements go here
 
 
 
 
 
-    private int index = -1;
+    public int curDeliveryIndex = 0;
+    public int curItemIndex = 0;
     private List<Delivery> deliveries = new List<Delivery>();
-    public Dictionary<string, GameObject> items = new Dictionary<string, GameObject>();
+
+
+    [SerializeField]
+    public List<GameObject> items = new List<GameObject>();
+    [SerializeField]
+    public List<string> itemKey = new List<string>();
+
     public GameObject curObject;
     public GameObject boss;
     // Use this for initialization
@@ -26,34 +117,83 @@ public class GameManager : MonoBehaviour
         #region Delivery Content
         Delivery d = new Delivery();
 
-        d.name = "China";
+        d.name = "Main";
         d.AddDialogue("Hello", ref boss);
         d.AddDialogue("We have a new Delivery", ref boss);
-        //d.AddItem(items["DVD Fake"]);
+
+
+
+
+        d.AddItem(items[itemKey.IndexOf("Cigarette Fake")]);
+        d.AddItem(items[itemKey.IndexOf("Cigarette Real")]);
+        d.AddItem(items[itemKey.IndexOf("DVD Fake")]);
+        d.AddItem(items[itemKey.IndexOf("DVD Real")]);
+
+
 
         deliveries.Add(d);
-
         #endregion
+
+
+        deliveries[curDeliveryIndex].active = true;
 
 
 
     }
 
-    // Update is called once per frame
+
     void Update()
     {
 
-        //TODO get the current delivery
-
-
-        if (!boss.GetComponent<BossController>().speaking)
+        if (Input.GetKeyDown(KeyCode.T))
         {
-            if (!deliveries[0].ProcessDialogue(boss.GetComponent<BossController>().curIndex))
+            curItemIndex++;
+
+        }
+        if (deliveries[curDeliveryIndex].active == true)
+        {
+
+            if (!boss.GetComponent<BossController>().speaking)
+            {
+                //TODO fix getting current delivery
+                if (!deliveries[curDeliveryIndex].ProcessDialogue(boss.GetComponent<BossController>().curIndex))
+                {
+                    if (boss.GetComponent<BossController>().state != BossController.BossState.EXIT)
+                    {
+                        boss.GetComponent<BossController>().BossExit();
+
+                    }
+                }
+            }
+
+            if (!deliveries[curDeliveryIndex].SetActiveItem(curItemIndex))
             {
 
-                boss.GetComponent<BossController>().BossExit();
+                deliveries[curDeliveryIndex].active = false;
+                curItemIndex = 0;
             }
+
         }
+
+        else if (deliveries[curDeliveryIndex].active == false)
+        {
+            if (curDeliveryIndex + 1 > deliveries.Count - 1)
+            {
+
+                //THis should be the end of the game I.E. the last delivery get the score here
+                return;
+            }
+            else
+            {
+                //Otherwise increment the delivery index
+                curDeliveryIndex++;
+                deliveries[curDeliveryIndex].active = true;
+
+            }
+
+        }
+
+        //curItemIndex++;
 
 
     }
