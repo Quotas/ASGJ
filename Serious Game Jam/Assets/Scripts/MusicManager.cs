b0a17d8391183866731f2eb93a2484c4a1e605e0
James McAdam <j.g.mcadam@gmail.com>
2017-05-18 19:11:36 +0100

    Outline for sound and endgame mods
    Got a lot to do!!! Woooo alll nighter

---
+++ Serious Game Jam/Assets/Scripts/MusicManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour {
+
+	public AudioSource fxSource;
+	public AudioSource musicSource;
+
+	// Use this for initialization
+	void Start () 
+	{
+
+		// Don't interupt the music during restart or reload of the scene
+		DontDestroyOnLoad (this);
+
+	}
+
+
+	public void SinglePlay(AudioClip clip)
+	{
+
+	}
+
+}
